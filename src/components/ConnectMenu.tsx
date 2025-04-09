ff617429223e460db23baa08877c18de858dc2ba
You Song Hou <ysongweb3@gmail.com>
2025-04-09 17:31:26 -0400

    style: add styles to the button

--- src/components/ConnectMenu.tsx
+++ src/components/ConnectMenu.tsx
@@ -15,7 +15,11 @@ export function ConnectMenu() {
   }
 
   return (
-    <button className="bg-blue-400 cursor-pointer" type="button" onClick={() => connect({ connector: connectors[0] })}>
+    <button
+      type="button"
+      onClick={() => connect({ connector: connectors[0] })}
+      className="w-full py-2 px-4 bg-indigo-600 text-white font-medium rounded hover:bg-indigo-700 disabled:bg-indigo-300 disabled:cursor-not-allowed"
+    >
       Connect
     </button>
   );
@@ -26,7 +30,12 @@ function SignButton() {
 
   return (
     <>
-      <button className="bg-blue-400" type="button" onClick={() => signMessage({ message: "hello world" })} disabled={isPending}>
+      <button
+        type="button"
+        onClick={() => signMessage({ message: "hello world" })}
+        disabled={isPending}
+        className="w-full py-2 px-4 bg-indigo-600 text-white font-medium rounded hover:bg-indigo-700 disabled:bg-indigo-300 disabled:cursor-not-allowed"
+      >
         {isPending ? "Signing..." : "Sign message"}
       </button>
       {data && (
