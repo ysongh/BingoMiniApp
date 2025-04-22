18d1adcee5799369e56c4d26bb6cba8c946606a8
You Song Hou <ysongweb3@gmail.com>
2025-04-21 22:30:44 -0400

    feat: add connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,26 +1,32 @@
 import express from 'express';
- import cors from 'cors';
- import dotenv from 'dotenv';
- import pkg from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pkg from 'body-parser';
+import mongoose from 'mongoose';
  
- dotenv.config();
- 
- const { json } = pkg;
- const app = express();
- const port = 4000;
- 
- app.use(cors());
- 
- app.use(json());
- 
- app.get('/', (req, res) => res.send('It Work'));
- 
- app.use((err, req, res, next) => {
-     console.error(err.stack);
-     res.status(500).json({ message: 'Something went wrong!' });
- });
- 
- app.listen(port, () => {
-     console.log(`Server is running on port ${port}`);
- });
- 
+dotenv.config();
+
+const { json } = pkg;
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => res.send('It Work'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
